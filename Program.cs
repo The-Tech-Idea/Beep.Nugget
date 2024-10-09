12b12fece199f79f5353f2099eb1c04f76616de7
Fahed Al-Dhubaib <fahad@thetechidea.com>
2024-10-09 10:32:58 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Beep.Nugget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
